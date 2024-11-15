5455df7a75e05d086973ed48f156cd2a5691c9e1
nyamanaka <natsuki-yamanaka@binc.jp>
2024-11-15 10:17:24 +0900

    invalid sample code

--- lib/Net/Payjp.pm
+++ lib/Net/Payjp.pm
@@ -453,7 +453,7 @@ Finish 3D-Secure flow of token
 
 L<https://pay.jp/docs/api/#%E3%83%88%E3%83%BC%E3%82%AF%E3%83%B3%E3%81%AB%E5%AF%BE%E3%81%99%E3%82%8B3d%E3%82%BB%E3%82%AD%E3%83%A5%E3%82%A2%E3%83%95%E3%83%AD%E3%83%BC%E3%82%92%E5%AE%8C%E4%BA%86%E3%81%99%E3%82%8B>
 
- $payjp->id->('tok_xxxxxx');
+ $payjp->id('tok_xxxxxx');
  $payjp->token->tds_finish();
 
 =cut
